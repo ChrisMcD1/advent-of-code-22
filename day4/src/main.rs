d939a3cfddf72454b90183219fd5001f34f95f21
Chris McDonnell <c.a.mcdonn@gmail.com>
2022-12-04 10:22:36 -0500

    Change type to struct

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,23 +1,52 @@
 use std::fs;
 fn main() {
     println!("Hello, world!");
-    part_1();
-    part_2();
+    let input = fs::read_to_string("./src/input.txt").unwrap();
+    let count = part_1(&input);
+    println!("Part 1: {:?}", count);
 }
 
-fn part_1() {
-    let input = fs::read_to_string("input.txt").unwrap();
+struct RangeStruct {
+    left: i32,
+    right: i32,
+}
+
+impl RangeStruct {
+    fn from_str(input: &str) -> Option<Self> {
+        let mut split_string: Vec<&str> = input.split('-').collect();
+        let left = match str::parse::<i32>(split_string[0]) {
+            Ok(value) => value,
+            Err(_) => return None,
+        };
+        let right = match str::parse::<i32>(split_string[1]) {
+            Ok(value) => value,
+            Err(_) => return None,
+        };
+        return Some(RangeStruct { left, right });
+    }
+}
+
+type Range = (i32, i32);
+
+fn split_range(string: &str) -> Range {
+    let mut split_string: Vec<&str> = string.split('-').collect();
+    let second_num = str::parse::<i32>(split_string.pop().unwrap()).unwrap();
+    let first_num = str::parse::<i32>(split_string.pop().unwrap()).unwrap();
+    return (first_num, second_num);
+}
+
+fn part_1(input: &str) -> u32 {
     let pairs: Vec<&str> = input.split("\n").collect();
-    let count: i32 = pairs
+    let count: u32 = pairs
         .iter()
         .filter(|&&pair| !pair.eq(""))
         .map(|pair| {
             let mut split: Vec<&str> = pair.split(",").collect();
-            let first = split_range(split.pop().unwrap());
-            let second = split_range(split.pop().unwrap());
-            if (first.1 >= second.1 && first.0 <= second.0) {
+            let first = RangeStruct::from_str(split.pop().unwrap()).unwrap();
+            let second = RangeStruct::from_str(split.pop().unwrap()).unwrap();
+            if (first.right >= second.right && first.left <= second.left) {
                 return 1;
-            } else if (second.1 >= first.1 && second.0 <= first.0) {
+            } else if (second.right >= first.right && second.left <= first.left) {
                 return 1;
             } else {
                 return 0;
@@ -25,7 +54,7 @@ fn part_1() {
         })
         .sum();
 
-    println!("Part 1: {:?}", count);
+    return count;
 }
 
 fn part_2() {
@@ -52,13 +81,20 @@ fn part_2() {
     //    println!("Part 1: {:?}");
 }
 
-type Range = (i32, i32);
+#[cfg(test)]
+mod test {
+    use std::fs;
 
-fn split_range(string: &str) -> Range {
-    let mut split_string: Vec<&str> = string.split('-').collect();
-    let second_num = str::parse::<i32>(split_string.pop().unwrap()).unwrap();
-    let first_num = str::parse::<i32>(split_string.pop().unwrap()).unwrap();
-    return (first_num, second_num);
+    use crate::part_1;
+
+    #[test]
+    fn aoc_solution() {
+        let input = fs::read_to_string("./src/input.txt").unwrap();
+
+        let count = part_1(&input);
+
+        assert_eq!(count, 441);
+    }
 }
 
 fn pair_overlap(first: &Range, second: &Range) -> i32 {
