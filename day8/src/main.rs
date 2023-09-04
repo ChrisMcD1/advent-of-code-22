887f7c82c284c599e32aaff10c34b9dbefb3e5f4
Chris McDonnell <camcdonn@gmail.com>
2023-09-04 01:50:48 -0400

    Made changes to make day 8 more idiomatic

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,19 +1,25 @@
-use std::time::Instant;
+#![deny(clippy::unwrap_used)]
+use std::{str::FromStr, time::Instant};
 
-use anyhow::Result;
 fn main() {
     println!("Hello, world!");
     let real_input = include_str!("./1_real");
 
-    let tree_count = part_1(real_input).unwrap();
+    if let Ok(tree_count) = part_1(real_input) {
+        println!("There are {tree_count} trees");
+    } else {
+        println!("Failed to parse input 1");
+    }
 
-    println!("There are {tree_count} trees");
     let start = Instant::now();
-    let best_score = part_2(real_input);
-    let end = Instant::now();
-    let elapsed = end - start;
-    println!("Ran pant 2 in {:?}", elapsed);
-    println!("The best tree house has a score of {best_score}");
+    if let Ok(best_score) = part_2(real_input) {
+        let end = Instant::now();
+        let elapsed = end - start;
+        println!("Ran pant 2 in {:?}", elapsed);
+        println!("The best tree house has a score of {best_score}");
+    } else {
+        println!("Failed on input 2");
+    }
 }
 
 #[derive(Debug, Copy, Clone, PartialEq)]
@@ -21,17 +27,57 @@ struct Tree {
     height: u32,
 }
 
-type Forest = Vec<Vec<Tree>>;
+#[derive(Debug, Clone, PartialEq)]
+struct Forest {
+    pub trees: Vec<Vec<Tree>>,
+}
+
+#[derive(PartialEq, Clone, Copy, Debug)]
+enum ForestParseError {
+    NonDigitCharacter,
+}
+
+impl FromStr for Forest {
+    type Err = ForestParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let trees = s.lines().map(parse_forest_line).collect::<Result<_, _>>()?;
+        Ok(Forest::new(trees))
+    }
+}
+
+fn parse_forest_line(line: &str) -> Result<Vec<Tree>, ForestParseError> {
+    line.chars()
+        .map(|char| {
+            let height = char
+                .to_digit(10)
+                .ok_or(ForestParseError::NonDigitCharacter)?;
+            Ok(Tree { height })
+        })
+        .collect()
+}
+
+impl Forest {
+    pub fn width(&self) -> usize {
+        self.trees[0].len()
+    }
+    pub fn height(&self) -> usize {
+        self.trees.len()
+    }
+    pub fn new(trees: Vec<Vec<Tree>>) -> Self {
+        Forest { trees }
+    }
+}
 
 fn is_tree_visible(forest: &Forest, x: usize, y: usize) -> bool {
-    let target_tree = forest[x][y];
+    let target_tree = forest.trees[x][y];
 
     let tree_slices = TreeVisiblitySlices::create_for_tree(forest, x, y);
 
-    return tree_visible_in_slice(&tree_slices.left, &target_tree)
+    tree_visible_in_slice(&tree_slices.left, &target_tree)
         || tree_visible_in_slice(&tree_slices.right, &target_tree)
         || tree_visible_in_slice(&tree_slices.up, &target_tree)
-        || tree_visible_in_slice(&tree_slices.down, &target_tree);
+        || tree_visible_in_slice(&tree_slices.down, &target_tree)
 }
 
 #[derive(Debug)]
@@ -44,116 +90,101 @@ struct TreeVisiblitySlices {
 
 impl TreeVisiblitySlices {
     fn create_for_tree(forest: &Forest, x: usize, y: usize) -> TreeVisiblitySlices {
-        let forest_width = forest[0].len();
-        let forest_height = forest.len();
         let left_of_tree: Vec<Tree> = if y == 0 {
             Vec::new()
         } else {
-            forest[x][..=y - 1].to_vec()
+            forest.trees[x][..=y - 1].to_vec()
         };
-        let right_of_tree: Vec<Tree> = if y == forest_width - 1 {
+        let right_of_tree: Vec<Tree> = if y == forest.width() - 1 {
             Vec::new()
         } else {
-            forest[x][y + 1..].to_vec()
+            forest.trees[x][y + 1..].to_vec()
         };
         let above_tree: Vec<Tree> = if x == 0 {
             Vec::new()
         } else {
-            forest[..=x - 1].iter().map(|row| row[y]).collect()
+            forest.trees[..=x - 1].iter().map(|row| row[y]).collect()
         };
-        let below_tree: Vec<Tree> = if x == forest_height - 1 {
+        let below_tree: Vec<Tree> = if x == forest.height() - 1 {
             Vec::new()
         } else {
-            forest[x + 1..].iter().map(|row| row[y]).collect()
+            forest.trees[x + 1..].iter().map(|row| row[y]).collect()
         };
-        return TreeVisiblitySlices {
+        TreeVisiblitySlices {
             left: left_of_tree,
             right: right_of_tree,
             up: above_tree,
             down: below_tree,
-        };
+        }
     }
 }
 
-fn tree_visible_in_slice(slice: &Vec<Tree>, target_tree: &Tree) -> bool {
-    return slice.iter().all(|tree| tree.height < target_tree.height);
+fn tree_visible_in_slice(slice: &[Tree], target_tree: &Tree) -> bool {
+    slice.iter().all(|tree| tree.height < target_tree.height)
 }
 
 fn tree_score(forest: &Forest, x: usize, y: usize) -> usize {
-    let target_tree = forest[x][y];
+    let target_tree = forest.trees[x][y];
 
     let mut tree_slices = TreeVisiblitySlices::create_for_tree(forest, x, y);
 
     tree_slices.left.reverse();
     tree_slices.up.reverse();
 
-    return tree_slice_score(&tree_slices.left, &target_tree)
-        * tree_slice_score(&tree_slices.right, &target_tree)
-        * tree_slice_score(&tree_slices.up, &target_tree)
-        * tree_slice_score(&tree_slices.down, &target_tree);
+    trees_visible_in_line(&tree_slices.left, &target_tree)
+        * trees_visible_in_line(&tree_slices.right, &target_tree)
+        * trees_visible_in_line(&tree_slices.up, &target_tree)
+        * trees_visible_in_line(&tree_slices.down, &target_tree)
 }
 
-fn tree_slice_score(slice: &Vec<Tree>, target_tree: &Tree) -> usize {
-    let tree_visible_count = match slice
+fn trees_visible_in_line(line: &[Tree], target_tree: &Tree) -> usize {
+    match line
         .iter()
         .position(|tree| tree.height >= target_tree.height)
     {
         Some(tree_index) => tree_index + 1,
-        None => slice.len(),
-    };
-    return tree_visible_count;
+        None => line.len(),
+    }
 }
 
-fn part_1(input: &str) -> Result<usize> {
-    let forest = parse_forest(input);
+fn part_1(input: &str) -> Result<usize, ForestParseError> {
+    let forest = Forest::from_str(input)?;
 
-    let forest_width = forest[0].len();
-    let forest_height = forest.len();
-    println!("Tree is {forest_width} wide and {forest_height} tall");
-    let trees_visible_count = (0..forest_height)
+    println!(
+        "Tree is {} wide and {} tall",
+        forest.width(),
+        forest.height()
+    );
+    let trees_visible_count = (0..forest.height())
         .map(|x| {
-            (0..forest_width)
+            (0..forest.width())
                 .map(|y| is_tree_visible(&forest, x, y))
                 .filter(|&is_visible| is_visible)
                 .count()
         })
         .sum();
 
-    return Ok(trees_visible_count);
+    Ok(trees_visible_count)
 }
 
-fn part_2(input: &str) -> usize {
-    let forest = parse_forest(input);
+fn part_2(input: &str) -> Result<usize, ForestParseError> {
+    let forest = Forest::from_str(input)?;
 
-    let forest_width = forest[0].len();
-    let forest_height = forest.len();
-    println!("Tree is {forest_width} wide and {forest_height} tall");
-    let max_tree_score = (0..forest_height)
+    println!(
+        "Tree is {} wide and {} tall",
+        forest.width(),
+        forest.height()
+    );
+
+    Ok((0..forest.height())
         .map(|x| {
-            (0..forest_width)
+            (0..forest.width())
                 .map(|y| tree_score(&forest, x, y))
                 .max()
-                .unwrap()
+                .unwrap_or(0)
         })
         .max()
-        .unwrap();
-
-    return max_tree_score;
-}
-
-fn parse_forest(input: &str) -> Forest {
-    let forest: Forest = input
-        .lines()
-        .map(|line| {
-            line.chars()
-                .map(|char| Tree {
-                    height: char.to_digit(10).unwrap(),
-                })
-                .collect()
-        })
-        .collect();
-
-    return forest;
+        .unwrap_or(0))
 }
 
 #[cfg(test)]
@@ -172,12 +203,12 @@ mod test {
     #[test]
     fn parse_forest_simple() {
         let sample_forest_str = "123\n456";
-        let expected_forest = vec![
+        let expected_forest = Forest::new(vec![
             vec![Tree { height: 1 }, Tree { height: 2 }, Tree { height: 3 }],
             vec![Tree { height: 4 }, Tree { height: 5 }, Tree { height: 6 }],
-        ];
+        ]);
 
-        let forest = parse_forest(sample_forest_str);
+        let forest = Forest::from_str(sample_forest_str).unwrap();
 
         println!("{:?}", forest);
         println!("{:?}", expected_forest);
@@ -185,9 +216,9 @@ mod test {
         assert_eq!(two_forests_equal(forest, expected_forest), true);
     }
     fn two_forests_equal(first: Forest, second: Forest) -> bool {
-        first.iter().enumerate().all(|(index, first_row)| {
+        first.trees.iter().enumerate().all(|(index, first_row)| {
             println!("first_row: {:?}", first_row);
-            let second_row = &second[index];
+            let second_row = &second.trees[index];
             println!("second_row: {:?}", second_row);
             first_row
                 .iter()
@@ -197,10 +228,10 @@ mod test {
 
     #[test]
     fn tree_edge_visible() {
-        let forest = vec![
+        let forest = Forest::new(vec![
             vec![Tree { height: 1 }, Tree { height: 2 }, Tree { height: 3 }],
             vec![Tree { height: 4 }, Tree { height: 5 }, Tree { height: 6 }],
-        ];
+        ]);
 
         let tree_visibility = is_tree_visible(&forest, 0, 1);
 
@@ -208,11 +239,11 @@ mod test {
     }
     #[test]
     fn tree_is_visible() {
-        let forest = vec![
+        let forest = Forest::new(vec![
             vec![Tree { height: 1 }, Tree { height: 2 }, Tree { height: 3 }],
             vec![Tree { height: 4 }, Tree { height: 10 }, Tree { height: 6 }],
             vec![Tree { height: 4 }, Tree { height: 5 }, Tree { height: 1 }],
-        ];
+        ]);
 
         let tree_visibility = is_tree_visible(&forest, 1, 1);
 
@@ -220,11 +251,11 @@ mod test {
     }
     #[test]
     fn tree_hidden() {
-        let forest = vec![
+        let forest = Forest::new(vec![
             vec![Tree { height: 1 }, Tree { height: 2 }, Tree { height: 3 }],
             vec![Tree { height: 4 }, Tree { height: 0 }, Tree { height: 6 }],
             vec![Tree { height: 4 }, Tree { height: 5 }, Tree { height: 1 }],
-        ];
+        ]);
 
         let tree_visibility = is_tree_visible(&forest, 1, 1);
 
@@ -232,11 +263,11 @@ mod test {
     }
     #[test]
     fn tree_score_basic() {
-        let forest = vec![
+        let forest = Forest::new(vec![
             vec![Tree { height: 1 }, Tree { height: 2 }, Tree { height: 3 }],
             vec![Tree { height: 4 }, Tree { height: 7 }, Tree { height: 6 }],
             vec![Tree { height: 4 }, Tree { height: 5 }, Tree { height: 1 }],
-        ];
+        ]);
 
         let tree_score = tree_score(&forest, 1, 1);
 
@@ -244,12 +275,12 @@ mod test {
     }
     #[test]
     fn tree_score_longer() {
-        let forest = vec![
+        let forest = Forest::new(vec![
             vec![Tree { height: 1 }, Tree { height: 2 }, Tree { height: 3 }],
             vec![Tree { height: 10 }, Tree { height: 7 }, Tree { height: 6 }],
             vec![Tree { height: 4 }, Tree { height: 5 }, Tree { height: 1 }],
             vec![Tree { height: 4 }, Tree { height: 10 }, Tree { height: 1 }],
-        ];
+        ]);
 
         let tree_score = tree_score(&forest, 1, 1);
 
@@ -259,7 +290,7 @@ mod test {
     fn part_2_given() {
         let input = include_str!("./1_given");
 
-        let max_count = part_2(input);
+        let max_count = part_2(input).unwrap();
 
         assert_eq!(max_count, 8);
     }
@@ -268,7 +299,7 @@ mod test {
     fn part_2_full() {
         let input = include_str!("./1_real");
 
-        let max_count = part_2(input);
+        let max_count = part_2(input).unwrap();
 
         assert_eq!(max_count, 345744);
     }
